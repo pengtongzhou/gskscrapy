3915886dfbb4c16fb75a8ce6e71aff04d98760cc
riverpath <643176574@qq.com>
2017-08-19 16:57:17 +0800

    通用爬虫

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import fetchtool.Asyncfetch as myscrapy
 from sys import argv
 import pdb,time
-# 从api之中获取模块
-
+#从dos环境中获取argv
 file_main,theme=argv
+
+#创建一个类并导入.josn、theme的相关性息
 URLS=myscrapy.webclass()
 URLS.setconfig(theme)
 URLS.seturl(URLS.cnf['argurl'])
@@ -14,5 +15,5 @@ myscrapy.fetch_text(URLS.urls,URLS.xslt,URLS.cnf['code'],URLS.cnf['cookie'],them
 # f=lambda a:map(lambda b:a[b:b+4],range(0,len(a),4))
 # arv=f(URLS.urls)
 # for i in arv:
-#     myscrapy.fetch_text(URLS.urls,URLS.xslt,URLS.cnf['code'],URLS.cnf['cookie'],theme)
+#     myscrapy.fetch_text(i,URLS.xslt,URLS.cnf['code'],URLS.cnf['cookie'],theme)
 #     time.sleep(15)
